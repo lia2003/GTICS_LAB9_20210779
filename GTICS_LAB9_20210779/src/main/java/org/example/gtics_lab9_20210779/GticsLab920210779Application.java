ec6efb2f096894b4433a52c8db726e2569086a4c
lia2003 <a20210779@pucp.edu.pe>
2024-11-10 14:04:56 -0500

    aksds

---
+++ GTICS_LAB9_20210779/src/main/java/org/example/gtics_lab9_20210779/GticsLab920210779Application.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab9_20210779;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab920210779Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab920210779Application.class, args);
+	}
+
+}
---
+++ GTICS_LAB9_20210779/src/test/java/org/example/gtics_lab9_20210779/GticsLab920210779ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab9_20210779;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab920210779ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
